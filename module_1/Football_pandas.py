ede6172a49742d6f67431c517a4c16b003d72ae3
Alexander Stratonov <the_steep@mail.ru>
2021-07-27 17:14:04 +0300

    Update Football_pandas.py

--- module_1/Football_pandas.py
+++ module_1/Football_pandas.py
@@ -5,5 +5,4 @@ reactions_max = 0.9 * football.Reactions.max()
 print(composure_max)
 print(reactions_max)
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    display(football[football.Composure > composure_max
-    & football.Reactions > reactions_max].Age.min())
+    display(football[(football.Composure > composure_max) & (football.Reactions > reactions_max)].Age.min())
