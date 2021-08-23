3231a5eb577e9445d6f600fbd1aa21a25b764046
Alexander Stratonov <the_steep@mail.ru>
2021-08-23 16:34:50 +0300

    1

--- module_1/Football_pandas.py
+++ module_1/Football_pandas.py
@@ -1,7 +1,7 @@
 import pandas as pd
-football = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    display(football[football.Age == football.Age.min()].Reactions.mean())
-    display(football[football.Age == football.Age.max()].Reactions.mean())
-    display((football[football.Age == football.Age.max()].Reactions.mean()) 
-            - (football[football.Age == football.Age.max()].Reactions.mean())
+df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
+small_df = df[df.columns[1:8]]
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#    display(small_df)
+s = df['FKAccuracy'].value_counts(bins=5)
+display(s)
--- module_1/untitled0.py
+++ module_1/untitled0.py
@@ -1,2 +1,10 @@
-a = ["asdsss", "bbd", "ddfa", "mcsa"]
-print(list(map(str.upper, a)))
+a1 = int(input())
+s = a1
+s2 = 0+a1**2
+while s!=0:
+    a1 = int(input())
+    s = s+a1
+    s2 = s2 + a1**2
+    if s==0:
+        break
+print(s2)
