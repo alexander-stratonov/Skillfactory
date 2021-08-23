4ff168e2a21936fba1b39f906f7d92c2c1e20cdc
alexander-stratonov <the_steep@mail.ru>
2021-08-23 21:35:52 +0300

    `

--- module_1/Football_pandas.py
+++ module_1/Football_pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
-#df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
-df = pd.read_csv('C:\Users\Крис\Documents\GitHub\Skillfactory_Alexander_Stratoonv/data_sf.csv')
+from IPython.display import display
+df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
 small_df = df[df.columns[1:8]]
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    display(small_df)
