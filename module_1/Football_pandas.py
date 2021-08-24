6affb6e66500b4b88aa87755857a1559bca23a08
alexander-stratonov <the_steep@mail.ru>
2021-08-24 21:04:45 +0300

    Update Football_pandas.py

--- module_1/Football_pandas.py
+++ module_1/Football_pandas.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from IPython.display import display
-df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
-#df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
-small_df = df[df.columns[1:8]]
+#df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
+df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
+small_df = df[df.columns[1:8]].head(25)
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    display(small_df)
-s = df['FKAccuracy'].value_counts(bins=5)
-display(s)
+s = small_df['Nationality'].value_counts()
+s_df = s.reset_index()
+s_df.columns = ['Nationality','Players Count']
+display(s_df)
