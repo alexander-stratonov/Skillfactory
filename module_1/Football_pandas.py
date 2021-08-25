f409569c1e9d5139961ba57ee4460a08928ef2d1
alexander-stratonov <the_steep@mail.ru>
2021-08-25 21:27:21 +0300

    Update Football_pandas.py

--- module_1/Football_pandas.py
+++ module_1/Football_pandas.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from IPython.display import display
-df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
-#df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
+#df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
+df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
 #small_df = df[df.columns[1:8]].head(25)
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    display(small_df)
-s = df.groupby(['Club']).groups
-display(s)
+df_groupby = df.groupby('Club')['Wage'].agg(['mean', 'median'])
+s = df_groupby.reset_index()
+s1 = s.groupby(['Club'])[['median', 'mean']].mean()
+display(s1)
