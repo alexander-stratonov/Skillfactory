e8b65b18e42a3af61aa850d6ba5bd0c92750beb5
alexander-stratonov <the_steep@mail.ru>
2021-08-26 21:24:09 +0300

    Update Football_pandas_task3.py

--- module_1/Football_pandas_task3.py
+++ module_1/Football_pandas_task3.py
@@ -4,6 +4,19 @@ from IPython.display import display
 df = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/data_sf.csv')
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    display(df)
-s = [(df.Nationality == 'Russia') & 
+#1
+s = df[(df.Nationality == 'Russia') & 
      (df.Club == "AS Monaco")].Wage.value_counts()
 display(s)
+#2
+s1 = df.pivot_table(values = ['SprintSpeed'],
+              index = ['Club'],
+              columns = ['Position'],
+              aggfunc = 'mean',
+              fill_value=0)
+s1 = s1['SprintSpeed']
+display(s1.max().sort_values(ascending=False).head(5))
+#3
+s2 = s1[(s1.Position == 'ST') & (s1.SprintSpeed > s1.SprintSpeed.mean())]
+display(s2)
+
