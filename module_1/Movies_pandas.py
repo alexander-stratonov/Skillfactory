21c8ad8cb542e10c11e18a0bf2a77e9f0c3f3d9e
Alexander Stratonov <the_steep@mail.ru>
2021-09-01 16:10:03 +0300

    ё

--- module_1/Movies_pandas.py
+++ module_1/Movies_pandas.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from IPython.display import display
-#df = pd.read_csv('C:/Users/user/Downloads/data_sf.csv')
-rating_pd = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/ratings.csv')
-movies_pd = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/movies.csv')
+#ratings = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/ratings.csv')
+#movies = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/movies.csv')
+ratings = pd.read_csv('C:/Users/user/Downloads/ratings.csv')
+movies = pd.read_csv('C:/Users/user/Downloads/movies.csv')
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    display(df)
-display(rating_pd)
-display(movies_pd)
-joined = rating_pd.merge(movies_pd, on='movieId', how='left')
+joined = ratings.merge(movies, on='movieId', how='left')
 with pd.option_context('display.max_columns', None):
     display(joined) 
+print(len(ratings) == len(joined))
