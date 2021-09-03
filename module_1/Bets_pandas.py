66465b35abdfc8f3af3b03e474e8ab057523ea9b
Alexander Stratonov <the_steep@mail.ru>
2021-09-03 12:03:04 +0300

    Update Bets_pandas.py

--- module_1/Bets_pandas.py
+++ module_1/Bets_pandas.py
@@ -1,9 +1,16 @@
 import pandas as pd
 from IPython.display import display
-users = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/users.csv')
-log = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/log.csv', header = None)
-#users = pd.read_csv('C:/Users/user/Downloads/users.csv')
-#log = pd.read_csv('C:/Users/user/Downloads/log.csv')
+#users = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/users.csv')
+#log = pd.read_csv('C:/Users/Крис/Documents/GitHub/Skillfactory_Alexander_Stratonov/log.csv', header = None)
+users = pd.read_csv('C:/Users/user/Downloads/users.csv', sep=';', encoding = 'koi8_r')
+#users.columns = ['user_id', 'email', 'geo']
+log = pd.read_csv('C:/Users/user/Downloads/log.csv', header = None)
+log.columns = ['user_id', 'time', 'bet', 'win']
 display(users)
 with pd.option_context('display.max_columns', None):
     display(log)
+
+sample = pd.read_csv('C:/Users/user/Downloads/sample.csv')
+display(sample)
+columns = sample.columns = ['name', 'city', 'age', 'profession']
+display(sample.info())
