dde2e1625ca35328c7ab22be5301595918f8d94c
Alexander Stratonov <the_steep@mail.ru>
2021-10-01 16:58:36 +0300

    Update EDA.py

--- module_2/EDA.py
+++ module_2/EDA.py
@@ -1,3 +1,8 @@
 import numpy as np
-arr1 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+import pandas as pd
+arr1 = np.array([1,2,3,4])
 print(arr1)
+arr2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr2)
+arr3 = np.array([5,6,7,8], dtype = float)
+print(arr3)
