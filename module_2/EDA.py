74b8393e36252c56d3f69dbe9d9242247eeb7357
Alexander Stratonov <the_steep@mail.ru>
2021-10-04 18:10:45 +0300

    `

--- module_2/EDA.py
+++ module_2/EDA.py
@@ -6,3 +6,24 @@ arr2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(arr2)
 arr3 = np.array([5,6,7,8], dtype = float)
 print(arr3)
+
+df = pd.DataFrame()
+x = df.values
+print(type(x))
+
+my_series = pd.Series([5, 6, 7, 8, 9, 10])
+display(my_series.values)
+
+display(np.empty(222)) # одномерный массив из пяти элементов, память для которого выделена, но не инициализирована
+display(np.zeros((10, 7))) # массив размером 10x7, заполненный нулями 
+display(np.ones((3,3,3))) # массив размером 3х3х3, заполненный единицами 
+display(np.eye(3)) # единичная матрица (элементы главной диагонали равны 1, остальные — 0) размера 3х3
+display(np.full((3, 5), 3.14))  # массив 3x5 заполненный числом 3.14
+display(np.arange(0, 21, 7))  # одномерный массив, заполненный числами в диапазоне от 0 до 20 с шагом 7
+display(np.linspace(0, 1, 5))  # массив из пяти чисел, равномерно распределённых в интервале между 0 и 1 включительно
+display(np.random.randint(0, 10, (3, 3)))  # массив размера 3х3, заполненный случайными числами из диапазона от 0 до 9 (включительно)
+
+my_secret = [x for x in range(1, 301, 7) if x%10 == 7 or x%10 == 1]
+display(np.array([my_secret, [x/2 for x in my_secret], [x-100 for x in my_secret]]))
+
+display(np.array([i for i in range(11) if i%2]))
---
+++ module_2/EDA_1.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+np.set_printoptions(precision=4,suppress=True)
+first_line = [x*y for x in range(2, 100, 6) for y in range (7, 1, -2)]
+second_line = [x ** 0.5 for x in range(1000, 1101, 2)]
+third_line = [x**2 for x in range(51)]
+big_secret = np.array([first_line, second_line, third_line, second_line, first_line])
+display(big_secret)
+
+#1
+a = big_secret[:,-1]
+a1 = a[0]+a[1]+a[2]+a[3]+a[4]
+print('1 =', a1)
+#2
+b= big_secret[:,0:5]
+b2 = 0
+for x in range(0,5):
+    j = b[x,x]
+    b2 = b2 + j
+print('2 =', b2)
+#3
+c= big_secret[:,-5:]
+c2 = 1
+for x in range(0,5):
+    j = c[x,x]
+    c2 = c2 * j
+print('3 =', c2)
+
+#_
+my_array = np.random.randint(1, 100, (4, 6)) 
+display(my_array)
+my_slice = my_array[1:3, 2:4]
+display(my_slice)
+my_slice[:] = 0
+display(my_array)
+
+#_
+big_secret[::2,::2] = -1
+big_secret[1::2, 1::2] = 1
+display(big_secret)
+
+#4
+d= big_secret[:,0:5]
+d2 = 0
+for x in range(0,5):
+    j = d[x,x]
+    d2 = d2 + j
+print('4 =', d2)
+
+#5
+e= big_secret[:,-5:]
+e2 = 1
+for x in range(0,5):
+    j = c[x,x]
+    e2 = e2 * j
+print('5 =', e2)
